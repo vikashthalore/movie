e636a7e07ba6716ba00bc7763180139dc5b8743d
vikashthalore <vikashthalor74@gmail.com>
2025-11-01 01:22:38 -0700

    Add new new add movie md style Change

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ const MovieCard = ({ movie }) => {
       <img
         src={movie.mainPoster || movie.imgSample?.[0] || "/placeholder.png"}
         alt={movie.title}
-        className="w-full h-82 object-cover transition-transform duration-300 group-hover:scale-105"
+        className="w-full h:68 md:h-82 object-cover transition-transform duration-300 group-hover:scale-105"
       />
       <div className="absolut bottom-0 bg-black/70 text-white w-full p-2 text-center text-sm border-amber-100">
         {movie.title}
